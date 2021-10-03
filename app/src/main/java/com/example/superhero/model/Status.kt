97b967f10085456206c5d061c22dcaae808fdbb3
Naufal Aliraqi <aliraqixnaufal1998@gmail.com>
2021-10-03 18:10:12 +0300

    feat: add code to client class and status class

--- app/src/main/java/com/example/superhero/model/Status.kt
+++ app/src/main/java/com/example/superhero/model/Status.kt
@@ -1,3 +1,7 @@
 package com.example.superhero.model
 
-sealed class Status
+sealed class Status<out T>{
+    object Loading : Status<Nothing>()
+    data class Error(val errorMessage : String) : Status<Nothing>()
+    data class Success<T>(val responseData: T) : Status<T>()
+}
--- app/src/main/java/com/example/superhero/model/network/Client.kt
+++ app/src/main/java/com/example/superhero/model/network/Client.kt
@@ -1,4 +1,39 @@
 package com.example.superhero.model.network
 
-interface Client {
+import android.util.Log
+import com.example.superhero.model.Status
+import com.example.superhero.model.responce.SuperHeroResponce
+import com.example.superhero.util.Constant
+import com.google.gson.Gson
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object Client {
+
+
+    private val okHttpClient = OkHttpClient()
+    private val gson = Gson()
+    private val superHeroUrl = HttpUrl.Builder()
+        .scheme("https")
+        .host("superheroapi.com")
+        .addPathSegment("/api/${Constant.KEY}/1")
+        .build()
+
+    fun makeSuperHeroRequest(): Status<SuperHeroResponce> {
+
+        val request = Request.Builder().url(superHeroUrl).build()
+        val response = okHttpClient.newCall(request).execute()
+
+        return if (response.isSuccessful) {
+            val parserResponse = gson.fromJson(
+                response.body?.string(),
+                SuperHeroResponce::class.java
+            )
+            Status.Success(parserResponse)
+        } else {
+            Status.Error(response.message)
+        }
+    }
+
 }
