cf6fdfe498506c124dc275e5b5672ed1fd44ce21
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-03 01:09:23 -0700

    after imp. the base activity

--- app/src/main/java/com/example/superhero/ui/Activities/HomeActivity.kt
+++ app/src/main/java/com/example/superhero/ui/Activities/HomeActivity.kt
@@ -1,12 +1,25 @@
 package com.example.superhero.ui.Activities
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import com.example.superhero.R
+import com.example.superhero.databinding.ActivityHomeBinding
 
-class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class HomeActivity() :BaseActivity<ActivityHomeBinding>() {
+
+
+    override val LOG_TAG: String = "Main_Activity"
+    override val bindingInflater: (LayoutInflater) -> ActivityHomeBinding
+            = ActivityHomeBinding::inflate
+
+    override fun setUp() {
+
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment).addToBackStack(null).commit()
+    }
+
+
 }
