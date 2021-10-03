a40dd2734a64ffa36917648d172f62aecbc40054
Noor ahmad <NourAhmedAbdulSattar41@gmail.com>
2021-10-03 07:05:30 -0700

    fix: details fragment

--- app/src/main/java/com/example/superhero/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/superhero/presenter/MainPresenter.kt
@@ -1,6 +1,6 @@
 package com.example.superhero.presenter
 
-import com.example.superhero.ui.homeActivity.InfoSuccess.IMainView
+import com.example.superhero.ui.IMainView
 
 class MainPresenter {
     lateinit var view : IMainView
