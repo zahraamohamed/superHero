d0fc4ca8b6972eb0118811d23290aefb92540ee2
zahraa resen <zaawory98@gmail.com>
2021-10-06 13:54:42 +0300

    add main presenter class

--- app/src/main/java/com/example/superhero/presenter/MainPresenter.kt
+++ app/src/main/java/com/example/superhero/presenter/MainPresenter.kt
@@ -1,7 +1,26 @@
 package com.example.superhero.presenter
 
+import com.example.superhero.model.Status
+import com.example.superhero.model.repository.MainRepository
 import com.example.superhero.ui.IMainView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class MainPresenter {
-    lateinit var view : IMainView
+class MainPresenter(private val view: IMainView) {
+    var repository = MainRepository()
+    fun getResult(searchHeroName:String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            repository.getInfoSuperHero(searchHeroName).collect { response ->
+
+                when (response) {
+                    is Status.Success -> view.onSuperheroResponseSuccess(data = response.responseData)
+
+            }
+        }
+    }
+}
+
+
 }
