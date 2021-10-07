40581ec382aacefaf82bf2643aea04feb0b83676
Karrar Jabbar <alioop137@gmail.com>
2021-10-07 21:23:50 +0300

    edit details bottom sheet

--- app/src/main/java/com/example/superhero/ui/fragment/details/DetailsFragment.kt
+++ app/src/main/java/com/example/superhero/ui/fragment/details/DetailsFragment.kt
@@ -1,36 +1,28 @@
 package com.example.superhero.ui.fragment.details
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.example.superhero.R
-import com.example.superhero.databinding.FragmentBiographyBinding
 import com.example.superhero.databinding.FragmentDetailsBinding
 import com.example.superhero.ui.fragment.BaseFragment
-import com.google.android.material.bottomsheet.BottomSheetDialog
-
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class DetailsFragment : BaseFragment<FragmentDetailsBinding>() {
-        override fun setup() {
-            showDetailsBottomSheet()
-        }
+    override fun setup() {
+        initBottomSheet()
+    }
 
-    private fun showDetailsBottomSheet() {
-        binding.showBottomSheetButton.setOnClickListener {
-
-            val dialog = BottomSheetDialog(requireContext())
-            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+    private fun initBottomSheet() {
+        BottomSheetBehavior.from(binding.bottomSheetSuperHero).apply {
+            peekHeight = 200
+            state = BottomSheetBehavior.STATE_COLLAPSED
         }
     }
 
     override fun callBack() {
-            TODO("Not yet implemented")
-        }
-
-        override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentDetailsBinding
-            get() = TODO("Not yet implemented")
-
-
+        TODO("Not yet implemented")
     }
+
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentDetailsBinding
+        get() = TODO("Not yet implemented")
+
+}
