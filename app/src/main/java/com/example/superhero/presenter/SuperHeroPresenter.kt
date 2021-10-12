b9b638b15cf1951ca01e7d0017fe061b780578da
Rafal A <rafal.akeel99@gmail.com>
2021-10-12 14:45:28 +0300

    declare coroutine scope and change the thread used in coroutine

--- app/src/main/java/com/example/superhero/presenter/SuperHeroPresenter.kt
+++ app/src/main/java/com/example/superhero/presenter/SuperHeroPresenter.kt
@@ -9,16 +9,6 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
-
-// TODO: 10/11/2021 your coroutineScope is used directly without any reference, so if we have another function in
-//presenter we couldn’t use same scope, it’s better to declare it inside your presenter.
-
-// TODO: 10/11/2021 custom scope use main dispatcher and that mean all of your request will run on MainThread,
-//coroutines is suspend and resume functions so if you make a request on main thread you will
-//not notice a UI lag unless there is a big response need to be parsed from json to objects, this
-//will drop some frames
-
-
 // TODO: 10/11/2021 we are wrapping our request with a state object to have a single source of truth. then why you
 //are unwrap the response state in SuperHeroPresenter and send everything to different functions in UI
 //layer. 
@@ -26,9 +16,10 @@ import kotlinx.coroutines.launch
 // TODO: 10/11/2021 - you need to add base presenter and make all presenters extend it 
 
 class SuperHeroPresenter(private val view: IMainView) {
-    var repository = MainRepository()
+    private var repository = MainRepository()
+    private var coroutineScope = CoroutineScope(Dispatchers.IO)
     fun getHeroResult(query: String) {
-        CoroutineScope(Dispatchers.Main).launch {
+        coroutineScope.launch {
             repository.getHeroData(query).catch {
                 view.onError()
             }.collect { response ->
